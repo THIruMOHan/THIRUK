3e9768dcc342efef3ec8af129297a1d6daacd217
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-02-02 14:06:10 +0530

    alphabet

--- alphabet.c
+++ alphabet.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char a;
+  printf("enter the value");
+  scanf('%c",&b);
+        if((b>='a')&&(b<='z'))
+        {
+          printf("its an alphabet");
+        }
+        else
+        {
+          printf("its number");
+     
+        }
+  getch();
+  return 0;
+        }
