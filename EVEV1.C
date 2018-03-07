7a641ff2f6997c1d9b57d45e02c1f039a2e0a683
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-03-07 15:55:13 +0530

    EVEN1.C

---
+++ EVEV1.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int n,m,p;
+	printf("enter the number1:");
+	scanf("%d",&n);
+	printf("\nenter the number2:");
+	scanf("%d",&m);
+	p=n*m;
+	if(P%2==0)
+	{
+		printf("\n even");
+	}
+	else
+	{
+		printf("\n odd");
+	}
+	return 0;
+}
+ 
